c65c18873ea5d795e9c2ed6a1cbb8a311dc2d7e1
tmyie <hello@tomjhume.com>
2014-02-25 13:47:59 +0000

    Added related posts

---
+++ WordPress : PHP/related_posts.php
@@ -0,0 +1,23 @@
+<?php
+//for use in the loop, list 5 post titles related to first tag on current post
+$tags = wp_get_post_tags($post->ID);
+if ($tags) {
+echo 'Related Posts';
+$first_tag = $tags[0]->term_id;
+$args=array(
+'tag__in' => array($first_tag),
+'post__not_in' => array($post->ID),
+'posts_per_page'=>5,
+'caller_get_posts'=>1
+);
+$my_query = new WP_Query($args);
+if( $my_query->have_posts() ) {
+while ($my_query->have_posts()) : $my_query->the_post(); ?>
+<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+
+<?php
+endwhile;
+}
+wp_reset_query();
+}
+?>
